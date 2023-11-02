329d835a69d78a53cdfb31f1d079d00d726a6a5c
motojouya <motojouya@gmail.com>
2023-11-02 09:55:36 +0900

    shell

--- resources/init.sh
+++ resources/init.sh
@@ -2,63 +2,72 @@
 set -x
 
 # definitions
-username=$1
-ssh_port=$2
-device=$3
-rdp_port=$4
+region=$1
+userid=$2
+username=$3
+password=$4
+ssh_port=$5
+volume_id=$6
 
-# start configurations
+export AWS_DEFAULT_REGION=$region
 cd /home/ec2-user
+instance_id=$(curl -s 169.254.169.254/latest/meta-data/instance-id)
 
-# mount disk
+# install commands
+yum update -y
+yum install -y nvme-cli
+yum install -y jq
+yum install -y tmux
+yum install -y tree
+yum install -y xauth
+yum install -y silversearcher-ag
+
+# mount ebs volume
+aws ec2 attach-volume --volume-id vol-$volume_id --instance-id $instance_id --device /dev/xvdb --region $region
+aws ec2 wait volume-in-use --volume-ids vol-$volume_id
+device=$(nvme list | grep $volume_id | awk '{print $1}' | xargs)
+while [ -z $device ]; do
+    sleep 1
+    device=$(nvme list | grep $volume_id | awk '{print $1}' | xargs)
+done
+# until [ -e $device ]; do
+#     sleep 1
+# done
 mkdir /home/$username
-mount /dev/$device /home/$username
+# mkfs -t ext4 $device
+mount $device /home/$username
+
+# add user
+useradd -u $userid -d /home/$username -s /bin/bash $username
+gpasswd -a $username sudo
+cp -arpf /home/ec2-user/.ssh/authorized_keys /home/$username/.ssh/authorized_keys
+chown $username /home/$username
+chgrp $username /home/$username
+chown -R $username /home/$username/.ssh
+chgrp -R $username /home/$username/.ssh
+echo "$username:$password" | chpasswd
 
 # ssh config
-curl https://raw.githubusercontent.com/motojouya/develop-ec2/main/gce/resources/sshd_config.tmpl -O
+curl https://raw.githubusercontent.com/motojouya/develop-ec2/main/resources/sshd_config.tmpl -O
 sed -e s/{%port%}/$ssh_port/g sshd_config.tmpl > sshd_config.init
 cp sshd_config.init /etc/ssh/sshd_config
 systemctl restart sshd
 
-apt-get update
-apt-get install -y jq
-apt-get install -y tmux
-apt-get install -y tree
-apt-get install -y xauth
-
-# apt-add-repository -y ppa:mizuno-as/silversearcher-ag
-# apt-get update -y
-apt-get install -y silversearcher-ag
-
-apt-get install -y nvme-cli
-
-# apt-get install -y nodejs
-# apt-get install -y npm
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt-get install -y nodejs
-npm install --global yarn
+# install nodejs
+yum install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
 npm install -g npx
 npm install -g typescript typescript-language-server
 
 # install docker
-apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg
-mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-gpasswd -a $username docker
-systemctl restart docker
-
-# curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
+amazon-linux-extras install docker
+service docker start
+systemctl enable docker
+usermod -a -G docker ec2-user
+usermod -a -G docker $username
+docker info
+# gpasswd -a $username docker
+# systemctl restart docker
 
 # # others
 # /home/$username/.fzf/install --bin
-
