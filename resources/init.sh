6cd2520681d60780efb80c2a184ec29c0d6429d8
motojouya <motojouya@gmail.com>
2023-12-13 09:00:15 +0900

    docker compose

--- resources/init.sh
+++ resources/init.sh
@@ -71,9 +71,17 @@ systemctl status docker
 usermod -aG docker $username
 docker info
 
-dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf --releasever=36 install -y docker-buildx-plugin docker-compose-plugin
+sudo dnf install -y docker
+sudo systemctl enable --now docker
+docker info
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-/usr/local/lib/docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 docker compose version
+# dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+# dnf --releasever=36 install -y docker-buildx-plugin docker-compose-plugin
 # gpasswd -a $username docker
 # systemctl restart docker
 
