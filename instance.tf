224c8de320ea00f7f414939a7c5e5fd41445766e
motojouya <motojouya@gmail.com>
2023-12-13 09:32:44 +0900

    delete comment

--- instance.tf
+++ instance.tf
@@ -1,35 +1,10 @@
-# data "aws_ami" "develop_ami" {
-#   owners           = ["amazon"]
-#   executable_users = ["self"]
-#   most_recent      = true
-# 
-#   filter {
-#     name   = "architecture"
-#     values = ["x86_64"] # , "arm64"
-#   }
-#   filter {
-#     name   = "root-device-type"
-#     values = ["ebs"]
-#   }
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-#   filter {
-#     name   = "name"
-#     values = ["${var.ami_name_prefix}*"]
-#   }
-# }
-
 resource "aws_instance" "develop" {
-    # ami           = data.aws_ami.develop_ami.id
     ami           = var.ami_id
     instance_type = var.instance_type
 
     associate_public_ip_address = true
     availability_zone           = var.availability_zone
     subnet_id                   = var.subnet_id
-    # security_groups             = ["${var.security_group_name}"]
     vpc_security_group_ids      = ["${var.security_group_id}"]
 
     key_name             = var.keypair_name
--- variable.tf
+++ variable.tf
@@ -4,19 +4,16 @@ variable "availability_zone" {}
 variable "subnet_id" {}
 variable "ssh_port" {}
 variable "security_group_id" {}
-# variable "security_group_name" {}
 
 # instance
 variable "instance_type" {}
 variable "max_price" {}
 variable "ami_id" {}
-# variable "ami_name_prefix" {}
 # variable "tags" {
 #   type        = list(string)
 # }
 
 # profiles
-# variable "user_id" {}
 variable "user_name" {}
 variable "keypair_name" {}
 variable "profile_name" {}
